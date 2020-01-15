04d849beccfc6c60ceaf2559dae5b4312c9c2071
Adam Broquet <adambroquet@gmail.com>
2020-01-15 16:42:07 +0100

    Create a pizza component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PizzaComponent } from './pizza/pizza.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/pizza.model.ts
@@ -0,0 +1,5 @@
+export class Pizza {
+    id: number;
+    name: string;
+    prix: number;
+  }
